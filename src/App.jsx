27d445cf377c2abbce5053b57ad7b838af56d0e4
Ajla Saranovic <ge43xed@mytum.de>
2023-10-11 16:19:01 +0200

    aylas-branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./HomePage.css";
 import HomePage from "./HomePage.jsx";
+import PokemonList from "./PokemonList.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/pokemon-list" element={<PokemonList />} />
       </Routes>
     </div>
   );
