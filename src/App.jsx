d6f8b9d71e02e247bbf47283f30fe099573de9ab
CallMeMo <addahri.mohamed297@gmail.com>
2023-10-12 14:48:00 +0200

    Battle

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/pokemon-list" element={<PokemonList />} />
+        
       </Routes>
     </div>
   );
